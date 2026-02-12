a74220c922858ecdfbbcca867b0cff4c43e2aaaa
Ekansh Bhushan <ekanshbhushan2k22@gmail.com>
2026-02-12 22:42:48 +0530

    Button added for navigation

--- app/src/main/java/eu/ekansh/wishlistapp/AppBar.kt
+++ app/src/main/java/eu/ekansh/wishlistapp/AppBar.kt
@@ -3,11 +3,16 @@ package eu.ekansh.wishlistapp
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -17,6 +22,16 @@ fun AppBarView(
     title : String,
     onBackNavClick : () -> Unit = {}
 ){
+
+    val navigationIcon : (@Composable () -> Unit) ?= {
+        IconButton(onClick = { onBackNavClick }) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                tint = Color.White,
+                contentDescription = null
+            )
+        }
+    }
     TopAppBar(
         title = {
             Text(
@@ -26,9 +41,10 @@ fun AppBarView(
         },
         modifier = Modifier
             .padding(start = 4.dp)
-            .heightIn(max = 24.dp),
+            .heightIn(max = 44.dp),
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 
 }
